8cc89cfaa55126956a3a6438b1be6fe29b79cf2b
Your Name <you@example.com>
2025-04-27 23:49:09 -0700

    Finished Ledger menu, Saving transactions

--- Capstone_1/src/main/java/Main.java
+++ Capstone_1/src/main/java/Main.java
@@ -1,9 +1,14 @@
+import org.example.Transaction;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean running = true;     // Keeps program running the loop
+        ArrayList<Transaction> transactions = new ArrayList<>();
 
         while (running) {          // 1. Main menu loop
             System.out.println("\nWelcome to the Ledger App");
@@ -14,24 +19,35 @@ public class Main {
             System.out.println("L) Ledger");
             System.out.println("X) Exit");
 
-            // Capture use input:
+            // 2. Capture use input:
             String choice = scanner.nextLine();
 
-            // Process user input
+            // 3. Process user input
             switch (choice.toUpperCase()) {
-                case "D":
-                    System.out.println("You chose Make Deposit.");
+                case "D":   // 4. Deposit function
+                    System.out.println("Enter deposit amount: ");
+                    String depositAmount = scanner.nextLine();
+                    Transaction deposit = new Transaction(depositAmount, "Deposit");
+                    transactions.add(deposit);
+                    System.out.println("You deposited: $" + depositAmount);
                     break;
-                case "P":
-                    System.out.println("You chose to Make Payment.");
+                case "P":   // 5. Payment function
+                    System.out.println("Enter payment amount: " );
+                    String paymentAmount = scanner.nextLine();
+                    Transaction payment = new Transaction(paymentAmount, "Payment");
+                    transactions.add(payment);
+                    System.out.println("You paid: $" + paymentAmount);
                     break;
-                case "L":
-                    System.out.println("You chose to view Ledger");
+                case "L":   // 6. Ledger function
+                    System.out.println("\n***** Ledger *****");
+                    for (Transaction t : transactions) {
+                        System.out.println(t.getType() + ": $" + t.getAmount());
+                    }
                     break;
-                case "X":
+                case "X":   // 7. Exit program
                     System.out.println("Exiting program.");
                     running = false;   // Stops the loop
-                    break;;
+                    break;
                 default:
                     System.out.println("Invalid choice. Please try again.");
 
--- Capstone_1/src/main/java/Transaction.java
+++ Capstone_1/src/main/java/Transaction.java
@@ -1,4 +1,21 @@
-package org.example;
-
 public class Transaction {
+    // 1. Variables
+    private String amount;
+    private String type;
+
+    // 2. Constructor
+    public Transaction(String amount, String type) {
+        this.amount = amount;
+        this.type = type;
+    }
+
+    // 3. Method for amount
+    public String getAmount() {
+        return amount;
+    }
+
+    // 4. Method for type
+    public String getType() {
+        return type;
+    }
 }
